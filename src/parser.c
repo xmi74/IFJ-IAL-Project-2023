62632b0362f0862bb5d50e329bc4401a3f26d10d
tomaar <tomaar19@gmail.com>
2023-10-18 21:10:46 +0200

    parser basic func

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,8 @@
 /**
  * IFJ-IAL Projekt 2023
  * 
- * @file scanner.c
- * @brief Implementacia programu pre lexikalnu analyzu
+ * @file parser.c
+ * @brief Implementacia programu syntakticke a semanticke analyzy
  * 
  * @author Igor Mikula (xmikul74)
  * @author Marko Olesak (xolesa00)
@@ -12,3 +12,46 @@
 
 #include "parser.h"
 
+
+keyword_t keyword_type(token_t token) // rozliseni identifikatoru a klicovych slov
+{
+    if (!strcmp(token.attribute.str.data, "func")) {
+        return FUNC;
+    }
+    else if (!strcmp(token.attribute.str.data, "if")) {
+        return IF;
+    }
+    else if (!strcmp(token.attribute.str.data, "else")) {
+        return ELSE;
+    }
+    else if (!strcmp(token.attribute.str.data, "while")) {
+        return WHILE;
+    }
+    else if (!strcmp(token.attribute.str.data, "for")) {
+        return FOR;
+    }
+    else {
+        return ID;
+    }
+}
+
+
+int parse()
+{
+    token_t currentToken;
+    currentToken = getNextToken();
+    while (currentToken.type != TOK_EOF) {
+        if (currentToken.type == TOK_COMMENT)
+        {
+            
+        }
+        else if (currentToken.type == TOK_IDENTIFIER)
+        {
+            keyword_type(currentToken);
+        }
+        
+
+        currentToken = getNextToken();
+    }
+    return 1;
+}
--- src/parser.h
+++ src/parser.h
@@ -1,13 +1,28 @@
 /**
  * IFJ-IAL Projekt 2023
  * 
- * @file scanner.h
- * @brief Implementacia programu pre lexikalnu analyzu
+ * @file parser.h
+ * @brief Implementacia programu syntakticke a semanticke analyzy
  * 
  * @author Igor Mikula (xmikul74)
  * @author Marko Olesak (xolesa00)
  * @author Jan Findra (xfindr01)
- * @author
+ * @author Tomas Arlt (xarltt00)
 */
 
-#include "scanner.h"
+#include "scanner.c"
+
+
+typedef enum keyword_type
+{
+    ID,
+    FUNC,
+    IF,
+    ELSE,
+    WHILE,
+    FOR
+} keyword_t;
+
+keyword_t keyword_type(token_t token);
+
+int parse();
