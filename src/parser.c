6e0d4bb3c087b3dc7eae2f2cbd2e979434aed549
tomaar <tomaar19@gmail.com>
2023-10-18 17:34:31 +0200

    parser init

---
+++ src/parser.c
@@ -0,0 +1,14 @@
+/**
+ * IFJ-IAL Projekt 2023
+ * 
+ * @file scanner.c
+ * @brief Implementacia programu pre lexikalnu analyzu
+ * 
+ * @author Igor Mikula (xmikul74)
+ * @author Marko Olesak (xolesa00)
+ * @author Jan Findra (xfindr01)
+ * @author Tomas Arlt (xarltt00)
+*/
+
+#include "parser.h"
+
---
+++ src/parser.h
@@ -0,0 +1,11 @@
+/**
+ * IFJ-IAL Projekt 2023
+ * 
+ * @file scanner.h
+ * @brief Implementacia programu pre lexikalnu analyzu
+ * 
+ * @author Igor Mikula (xmikul74)
+ * @author Marko Olesak (xolesa00)
+ * @author Jan Findra (xfindr01)
+ * @author
+*/
