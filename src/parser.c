ecc6a22e55dd39e4281bfad5d6255b994075f87f
tomaar <tomaar19@gmail.com>
2023-10-23 17:49:32 +0200

    remove keyword_type differentiation

--- src/parser.c
+++ src/parser.c
@@ -13,63 +13,65 @@
 #include "parser.h"
 
 
-keyword_t keyword_type(token_t token) // rozliseni identifikatoru a klicovych slov
-{
-    if (!strcmp(token.attribute.str.data, "Double")) {
-        return kw_Double;
-    }
-    else if (!strcmp(token.attribute.str.data, "else")) {
-        return kw_else;
-    }
-    else if (!strcmp(token.attribute.str.data, "func")) {
-        return kw_func;
-    }
-    else if (!strcmp(token.attribute.str.data, "if")) {
-        return kw_if;
-    }
-    else if (!strcmp(token.attribute.str.data, "Int")) {
-        return kw_Int;
-    }
-    else if (!strcmp(token.attribute.str.data, "let")) {
-        return kw_let;
-    }
-    else if (!strcmp(token.attribute.str.data, "nil")) {
-        return kw_nil;
-    }
-    else if (!strcmp(token.attribute.str.data, "return")) {
-        return kw_return;
-    }
-    else if (!strcmp(token.attribute.str.data, "String")) {
-        return kw_String;
-    }
-    else if (!strcmp(token.attribute.str.data, "while")) {
-        return kw_while;
-    }
-    else if (!strcmp(token.attribute.str.data, "var")) {
-        return kw_var;
-    }
-    else if (!strcmp(token.attribute.str.data, "while")) {
-        return kw_func;
-    }
-    else {
-        return kw_id;
-    }
-}
+//keyword_t keyword_type(token_t token) // rozliseni identifikatoru a klicovych slov
+//{
+//    if (!strcmp(token.attribute.str.data, "Double")) {
+//        return kw_Double;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "else")) {
+//        return kw_else;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "func")) {
+//        return kw_func;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "if")) {
+//        return kw_if;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "Int")) {
+//        return kw_Int;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "let")) {
+//        return kw_let;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "nil")) {
+//        return kw_nil;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "return")) {
+//        return kw_return;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "String")) {
+//        return kw_String;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "while")) {
+//        return kw_while;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "var")) {
+//        return kw_var;
+//    }
+//    else if (!strcmp(token.attribute.str.data, "while")) {
+//        return kw_func;
+//    }
+//    else {
+//        return kw_id;
+//    }
+//}
 
 
 int handle_assignment(token_t token_assigner, global_symtab_t *global_table, local_symtab_t *local_table)
 {
-    token_t currentToken = getNextToken();
+    bool is_constant = false;
 
-    if (keyword_type(currentToken) != kw_id)
+    if (token_assigner.type == TOK_KW_LET)
     {
-        //error
+        is_constant = true;
     }
+
+    token_t currentToken = getNextToken();
 
     if (currentToken.type == TOK_COLON)
     {
         currentToken = getNextToken();
-        if (keyword_type(currentToken) != kw_Double && keyword_type(currentToken) != kw_Int && keyword_type(currentToken) != kw_String)
+        if (currentToken.type != TOK_KW_DOUBLE && currentToken.type != TOK_KW_INT && currentToken.type != TOK_KW_STRING)
         {
             //error
         }
@@ -120,13 +122,9 @@ int parse_block(int nest_level, global_symtab_t *global_table, local_symtab_t *l
             }
             continue;
         }
-        else if (currentToken.type == TOK_IDENTIFIER)
+        else if (currentToken.type == TOK_KW_LET || currentToken.type == TOK_KW_VAR)
         {
-            keyword_t currentKeyword = keyword_type(currentToken);
-            if (currentKeyword == kw_let || currentKeyword == kw_var)
-            {
-                handle_assignment(currentToken, global_table, local_table);
-            }
+            handle_assignment(currentToken, global_table, local_table);
             
         }
         currentToken = getNextToken();
--- src/parser.h
+++ src/parser.h
@@ -10,26 +10,27 @@
  * @author Tomas Arlt (xarltt00)
 */
 
+#include <stdbool.h>
 #include "symtable.c"
 
 
-typedef enum keyword_type
-{
-    kw_id,
-    kw_Double,
-    kw_else,
-    kw_func,
-    kw_if,
-    kw_Int,
-    kw_let,
-    kw_nil,
-    kw_return,
-    kw_String,
-    kw_var,
-    kw_while
-} keyword_t;
-
-keyword_t keyword_type(token_t token);
+//typedef enum keyword_type
+//{
+//    kw_id,
+//    kw_Double,
+//    kw_else,
+//    kw_func,
+//    kw_if,
+//    kw_Int,
+//    kw_let,
+//    kw_nil,
+//    kw_return,
+//    kw_String,
+//    kw_var,
+//    kw_while
+//} keyword_t;
+//
+//keyword_t keyword_type(token_t token);
 
 int handle_assignment(token_t token_assigner, global_symtab_t *global_table, local_symtab_t *local_table);
 
