c459d72496957e36c288179c24c2f093e14f75c8
tomaar <tomaar19@gmail.com>
2023-12-05 15:43:17 +0100

    fix test err3p7

--- tests/errorTestInputs/err3p7.swift
+++ tests/errorTestInputs/err3p7.swift
@@ -1,6 +1,6 @@
 // Nedefinovana funkcia v definicii funkcie
 
 func a(_ value: Int) -> Int {
-    value = performAction() // Chyba: Nedefinovaná funkcia
+    let value = performAction() // Chyba: Nedefinovaná funkcia
     return value
 }
